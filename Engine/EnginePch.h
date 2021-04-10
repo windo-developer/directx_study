2301b9b87223bccf25ccfa473e5d3c513f199598
windo-developer <yui050505@gmail.com>
2021-04-11 02:47:49 +0900

    set-up

---
+++ Engine/EnginePch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+#include <tchar.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+using namespace std;
+
+
---
+++ WindowsProject1/Game.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "Game.h"
+
+void Game::Init()
+{
+}
+
+void Game::Update()
+{
+}
