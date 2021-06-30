b7cdbdfdb6997f2be954719ee763ec8f6b9c6e6d
windo-developer <yui050505@gmail.com>
2021-06-30 17:51:17 +0900

    normal mapping

--- Engine/Material.h
+++ Engine/Material.h
@@ -15,8 +15,10 @@ struct MaterialParams
 {
 	void SetInt(uint8 index, int32 value) { intParams[index] = value; }
 	void SetFloat(uint8 index, float value) { floatParams[index] = value; }
+	void SetTexOn(uint8 index, int32 value) { texOnParams[index] = value; }
 
 	array<int32, MATERIAL_INT_COUNT> intParams;
+	array<int32, MATERIAL_TEXTURE_COUNT> texOnParams;
 	array<float, MATERIAL_FLOAT_COUNT> floatParams;
 };
 
@@ -31,7 +33,11 @@ public:
 	void SetShader(shared_ptr<Shader> shader) { _shader = shader; }
 	void SetInt(uint8 index, int32 value) { _params.SetInt(index, value); }
 	void SetFloat(uint8 index, float value) { _params.SetFloat(index, value); }
-	void SetTexture(uint8 index, shared_ptr<Texture> texture) { _textures[index] = texture; }
+	void SetTexture(uint8 index, shared_ptr<Texture> texture)
+	{
+		_textures[index] = texture;
+		_params.SetTexOn(index, (texture == nullptr ? 0 : 1));
+	}
 
 	void PushData();
 
@@ -40,4 +46,3 @@ private:
 	MaterialParams		_params;
 	array<shared_ptr<Texture>, MATERIAL_TEXTURE_COUNT> _textures;
 };
-
